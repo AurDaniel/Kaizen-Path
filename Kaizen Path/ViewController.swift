5b7b146865c909be98924714622adee23a4edbd9
Aur Daniel <aurdaniel@ya.ru>
2023-09-13 01:39:31 +0300

    Timer
    Start work to timer.
    For start, it counts down from 5 to 0.

--- Kaizen Path/ViewController.swift
+++ Kaizen Path/ViewController.swift
@@ -4,17 +4,22 @@ class ViewController: UIViewController {
     
     let shapeLayer = CAShapeLayer()
     let trackLayer = CAShapeLayer()
+    var timeRemaning: Int = 5
+    var timer: Timer!
+    
+    @IBOutlet weak var timerLabel: UILabel!
     
     @IBAction func didTapStartButton(_ sender: UIButton) {
         
         let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
         basicAnimation.toValue = 1
-        basicAnimation.duration = 3
+        basicAnimation.duration = 6
         basicAnimation.fillMode = CAMediaTimingFillMode.forwards
         basicAnimation.isRemovedOnCompletion = false
         
         shapeLayer.add(basicAnimation, forKey: "start")
-        print("Нажал на кнопку")
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(step), userInfo: nil, repeats: true)
     }
     
     override func viewDidLoad() {
@@ -22,7 +27,8 @@ class ViewController: UIViewController {
         
         view.layer.addSublayer(trackLayer)
         view.layer.addSublayer(shapeLayer)
-
+        
+        timerLabel.text = "5"
         let center = view.center
         
         let circularPath = UIBezierPath(arcCenter: center, radius: 150, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
@@ -39,6 +45,17 @@ class ViewController: UIViewController {
         shapeLayer.strokeEnd = 0
         shapeLayer.fillColor = UIColor.clear.cgColor
         shapeLayer.lineCap = CAShapeLayerLineCap.round
+        
+    }
+    
+    @objc func step() {
+        if timeRemaning > 0 {
+            timeRemaning -= 1
+        } else {
+            timer.invalidate()
+            timeRemaning = 5
+        }
+        timerLabel.text = "\(timeRemaning)"
         
     }
     
